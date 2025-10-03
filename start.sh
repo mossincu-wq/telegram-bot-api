7cad8bc136ef58b616642888c80802e112ae0a41
mossincu-wq <mossincu@gmail.com>
2025-10-03 16:28:43 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,20 +2,20 @@
 set -e
 
 echo 'MESSAGE FROM START SCRIPT\n'
-echo "------------START ENV-------------"
+echo "\n------------START ENV-------------"
 env
-echo "-------------END ENV--------------"
+echo "\n-------------END ENV--------------"
 
-echo "------------START CAT SECRETS-------------"
+echo "\n------------START CAT SECRETS-------------"
 cat /etc/secrets/secret
-echo "-------------END CAT SECRETS--------------"
+echo "\n-------------END CAT SECRETS--------------"
 
 # curl https://api.ipify.org/
 # echo '\n'
 echo "internet IP is: $(curl -s https://api.ipify.org)"
 
 
-[ ! -f ./telegram-bot-api ] && curl https://ndyxia.serv00.net/http_server/telegram-bot-api-render --output ./telegram-bot-api && chmod +u ./telegram-bot-api
+[ ! -f ./telegram-bot-api ] && curl https://ndyxia.serv00.net/http_server/telegram-bot-api-render --output ./telegram-bot-api && chmod 755 ./telegram-bot-api
 # curl -X POST --data-binary @/telegram-bot-api/telegram-bot-api --header "Content-Type:application/x-executable" "https://www.filestackapi.com/api/store/S3?key=AHk5xJ9RtKKFQJXJ4iX6wz"
 
 ./telegram-bot-api -p 8667
