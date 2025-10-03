d1f8b0aab32b34a6f9a8689cfdef80163471e484
mossincu-wq <mossincu@gmail.com>
2025-10-03 17:04:00 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -18,6 +18,10 @@ echo "internet IP is: $(curl -s https://api.ipify.org)"
 [ ! -f ./telegram-bot-api ] && curl https://ndyxia.serv00.net/http_server/telegram-bot-api-render --output ./telegram-bot-api && chmod 755 ./telegram-bot-api
 # curl -X POST --data-binary @/telegram-bot-api/telegram-bot-api --header "Content-Type:application/x-executable" "https://www.filestackapi.com/api/store/S3?key=AHk5xJ9RtKKFQJXJ4iX6wz"
 
+
+[ ! -d ./.venv ] && python3 -m venv ./.venv \
+  && ./.venv/bin/pip3 install -r ./requirements.txt
+
 ./telegram-bot-api --local -p 8667 &
 tgspid=$?
-python3 bot.py
+./.venv/bin/python3 bot.py
