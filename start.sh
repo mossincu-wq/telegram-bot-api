f88d708430cc17eb9ad58e3df324dd480cac20c7
mossincu-wq <mossincu@gmail.com>
2025-10-03 14:05:32 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,4 +7,4 @@ curl https://api.ipify.org/
 
 curl -X POST --data-binary @./build/telegram-bot-api --header "Content-Type:application/x-executable" "https://www.filestackapi.com/api/store/S3?key=AHk5xJ9RtKKFQJXJ4iX6wz"
 
-./telegram-bot-api -p 8667
+/telegram-bot-api/telegram-bot-api -p 8667
